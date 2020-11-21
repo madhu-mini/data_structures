3775107634ca99b22c8f812cdb3980c8ede545cf
madhu-mini <madhuminiyar@gmail.com>
2020-11-22 00:47:28 +0530

    Adding new functions

--- Linked_List.py
+++ Linked_List.py
@@ -28,14 +28,13 @@ class LinkedList:
         print(temp.data, "->None")
         return True
 
-    def insert_at_head(self, data):
-        new_node = Node(data)
+    def insert_at_head(self, value):
+        new_node = Node(value)
         new_node.next_element = self.head_node
         self.head_node = new_node
         return self.head_node
 
     def insert_at_tail(self, value):
-        # Write - Your - Code
         new_node = Node(value)
         if self.is_empty():
             self.head_node = new_node
@@ -80,6 +79,104 @@ class LinkedList:
             prev_node = temp_node
             temp_node = temp_node.next_element
         return False
+
+    def length(self):
+        if self.head_node is None:
+            return 0
+        count = 0
+        temp_node = self.head_node
+        while temp_node:
+            count += 1
+            temp_node = temp_node.next_element
+        return count
+
+    def reverse(self):
+        if self.get_head() is not None:
+            temp_node = self.head_node
+            prev_node = None
+            next_node = None
+            while temp_node:
+                next_node = temp_node.next_element
+                temp_node.next_element = prev_node
+                prev_node = temp_node
+                temp_node = next_node
+            self.head_node = prev_node
+
+    def is_loop(self):
+        slow = self.get_head()
+        if slow:
+            fast = slow.next_element
+        while slow and fast and fast.next_element:
+            slow = slow.next_element
+            fast = fast.next_element.next_element
+            if slow == fast:
+                return True
+        return False
+
+    def find_mid(self):
+        if self.is_empty():
+            print("Empty List.")
+            return
+        slow = self.get_head()
+        fast = slow.next_element
+        while slow and fast and fast.next_element:
+            slow = slow.next_element
+            fast = fast.next_element.next_element
+        return slow.data
+
+    def remove_duplicates(self):
+        if self.is_empty():
+            print("Empty List.")
+            return
+        current = self.get_head()
+        prev = None
+        s = set()
+        while current:
+            if current.data in s:
+                temp_node = current
+                prev.next_element = temp_node.next_element
+                current = current.next_element
+                temp_node.next_element = None
+            else:
+                s.add(current.data)
+                prev = current
+                current = current.next_element
+
+    def union(self, new_list):
+        if new_list.is_empty():
+            return self.head_node
+        if self.is_empty():
+            return new_list
+        head_list = new_list.get_head()
+        while head_list.next_element:
+            head_list = head_list.next_element
+        head_list.next_element = self.get_head()
+        new_list.remove_duplicates()
+        return new_list
+
+    def intersection(self, new_list):
+        if self.is_empty() or new_list.is_empty():
+            return None
+        intersection_list = LinkedList()
+        head = self.get_head()
+        while head:
+            if new_list.search(head.data):
+                intersection_list.insert_at_tail(head.data)
+            head = head.next_element
+        return intersection_list
+
+    def get_nth_node_from_end(self, pos):
+        head = self.get_head()
+        while head and pos > 0:
+            head = head.next_element
+            pos -= 1
+        if pos:
+            return -1
+        temp = self.get_head()
+        while temp and head:
+            temp = temp.next_element
+            head = head.next_element
+        return temp.data
 
 
 def search_recursive(node, value):
